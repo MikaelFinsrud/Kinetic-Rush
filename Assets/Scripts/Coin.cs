930cf3f9e5f274df3dfb8dcc5f21fec332154231
Mikael Finsrud <mikael.lauvlid@hotmail.no>
2025-12-15 11:42:13 +0100

    feature/coin-shot

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,105 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public sealed class Coin : MonoBehaviour
+{
+    [Header("Refs")]
+    [SerializeField] private Rigidbody _rb;
+    [SerializeField] private PushPullTarget _pushPullTarget;
+
+    [Header("Tuning")]
+    [SerializeField] private float gravityMultiplier = 2f;
+    [SerializeField] private float impulseBackToPlayerBuffer = 0.2f;
+
+    private float _noImpulseBackToPlayerTime;
+    private bool canImpulseBackToPlayer = false;
+    public bool isAttached = false;
+
+    private void Awake()
+    {
+        GetComponent<AudioSource>().pitch = Random.Range(0.75f, 0.9f);
+    }
+
+    private void Start()
+    {
+        _pushPullTarget.OnPushed += HandleImpulse;
+    }
+
+    private void Reset()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 velocity = _rb.linearVelocity;
+        ApplyExtraGravity(ref velocity);
+        _rb.linearVelocity = velocity;
+    }
+
+    private void OnCollisionEnter(Collision c)
+    {
+        // Take first contact (you can average normals if you want)
+        AlignFlatToSurfaceNormal(c.GetContact(0).normal);
+
+        if (canImpulseBackToPlayer && Time.time <= _noImpulseBackToPlayerTime)
+        {
+            ImpulseBackToPlayer();
+            canImpulseBackToPlayer = false;
+        }
+    }
+
+    public void Launch(Vector3 velocity, float spinRadPerSec, Rigidbody launchParent)
+    {
+        transform.parent = null;
+        _rb.linearVelocity = velocity;
+        _rb.maxAngularVelocity = 70f;
+        _rb.angularVelocity = transform.right * spinRadPerSec;
+    }
+
+    public void AlignFlatToSurfaceNormal(Vector3 surfaceNormal)
+    {
+        // coinFaceNormal is the axis that points out of the coin's flat face.
+        Vector3 coinFaceNormal = transform.up; // change to transform.up / transform.right if needed
+
+        Quaternion delta = Quaternion.FromToRotation(coinFaceNormal, -surfaceNormal);
+
+        _rb.angularVelocity = Vector3.zero;
+        transform.rotation = delta * transform.rotation;
+    }
+
+    private void ApplyExtraGravity(ref Vector3 velocity)
+    {
+        Vector3 gravity = Physics.gravity * gravityMultiplier;
+        velocity += gravity * Time.fixedDeltaTime;
+    }
+
+    private void HandleImpulse()
+    {
+        _noImpulseBackToPlayerTime = Time.time + impulseBackToPlayerBuffer;
+        canImpulseBackToPlayer = true;
+    }
+
+    private void Detach() 
+    {
+        transform.parent = null;
+        _rb.constraints = RigidbodyConstraints.None;
+        isAttached = false;
+    }
+
+    public void Attach()
+    {
+        _rb.constraints = RigidbodyConstraints.FreezePosition;
+        isAttached = true;
+    }
+
+    private void ImpulseBackToPlayer()
+    {
+        if (PlayerPushPull.Instance != null)
+        {
+            Debug.Log("Coin Impulse Back To Player");
+            PlayerPushPull.Instance.ImpulseBackToPlayer(_pushPullTarget);
+        }
+    }
+}
--- Assets/Scripts/CoinShooter.cs
+++ Assets/Scripts/CoinShooter.cs
@@ -1,16 +1,71 @@
 using UnityEngine;
 
-public class CoinShooter : MonoBehaviour
+public sealed class CoinShooter : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Refs")]
+    private Camera _cam;
+    [SerializeField] private Transform _muzzle;
+    [SerializeField] private Coin _coinPrefab;
+    [SerializeField] private Collider[] _playerColliders;
+    [SerializeField] private Rigidbody _playerRigidbody;
+
+    [Header("Shoot Input")]
+    [SerializeField] private KeyCode shootKey = KeyCode.E;
+
+    [Header("Tuning")]
+    [SerializeField] private float _shootSpeed = 35f;
+    [SerializeField] private float _shootCooldown = 0.15f;
+    [SerializeField] private float _coinSpinRadPerSec = 20f;
+    [SerializeField] private float yInherit = 0.5f;
+
+    private float _nextShotTime;
+    private bool _shotQueued = false;
+    private Coin _currentCoin;
+
+    private void Awake()
     {
-        
+        _cam = Camera.main;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(shootKey))
+        {
+            _shotQueued = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (_shotQueued)
+        {
+            Shoot();
+            _shotQueued = false;
+        }
+    }
+
+    // Call this from your input layer (new Input System action, etc.)
+    public void Shoot()
+    {
+        if (_currentCoin != null)
+        {
+            Destroy(_currentCoin.gameObject); // Prevent more than one coin at a time
+        }
+
+        if (Time.time < _nextShotTime) return;
+        _nextShotTime = Time.time + _shootCooldown;
+
+        var coin = Instantiate(_coinPrefab, _muzzle.position, Quaternion.identity);
+        coin.transform.forward = _muzzle.forward;
+        _currentCoin = coin;
+
+        Vector3 inherited = Vector3.zero;
+        if (_playerRigidbody != null) 
+        { 
+            inherited = _playerRigidbody.GetPointVelocity(_muzzle.position); 
+            inherited = new Vector3(inherited.x, inherited.y * yInherit, inherited.z);
+        }
+
+        coin.Launch(inherited + (_muzzle.transform.forward * _shootSpeed), _coinSpinRadPerSec, _playerRigidbody);
     }
 }
